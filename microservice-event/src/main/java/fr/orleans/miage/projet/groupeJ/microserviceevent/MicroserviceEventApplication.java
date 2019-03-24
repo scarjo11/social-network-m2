15838c098857b7a7d899c1b8ecf4cb3ca87e67d6
Noutai Wilfrid Cesaire <wilfrid-cesaire.noutai@etu.uni-orleans.fr>
2019-03-24 19:47:13 +0100

    event opendata

---
+++ microservice-event/src/main/java/fr/orleans/miage/projet/groupeJ/microserviceevent/MicroserviceEventApplication.java
@@ -0,0 +1,13 @@
+package fr.orleans.miage.projet.groupeJ.microserviceevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceEventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceEventApplication.class, args);
+	}
+
+}
