32d5109836e0175c942acb42ae1446e19eefd41f
Gbahi Melaine <melaine.gbahi@etu.univ-orlenas>
2019-03-26 18:23:27 +0100

    architecture des differents componet de templates

--- microservice-client/src/main/resources/angular-template/src/app/app.module.ts
+++ microservice-client/src/main/resources/angular-template/src/app/app.module.ts
@@ -13,6 +13,18 @@ import {RouterModule, Routes} from "@angular/router";
 import { EventListComponent } from './event-list/event-list.component';
 import { ListUsersComponent } from './social-network/list-users/list-users.component';
 import { ListFriendsComponent } from './social-network/list-friends/list-friends.component';
+import { CreateOpenDataEventComponent } from './evenements/create-open-data-event/create-open-data-event.component';
+import { CreateEventPrivateComponent } from './evenements/create-event-private/create-event-private.component';
+import { CreateSoireeComponent } from './soiree/create-soiree/create-soiree.component';
+import { ListeParticipantSoireeComponent } from './soiree/liste-participant-soiree/liste-participant-soiree.component';
+import { AjouterParticipantSoireeComponent } from './soiree/ajouter-participant-soiree/ajouter-participant-soiree.component';
+import { SingleSoireeComponent } from './soiree/single-soiree/single-soiree.component';
+import { AjouterEventPriveSoireeComponent } from './soiree/ajouter-event-prive-soiree/ajouter-event-prive-soiree.component';
+import { AjouterEventOpendataSoireeComponent } from './soiree/ajouter-event-opendata-soiree/ajouter-event-opendata-soiree.component';
+import { EnvoyerNotificationComponent } from './notification/envoyer-notification/envoyer-notification.component';
+import { RecupererNotificationParPseudoComponent } from './notification/recuperer-notification-par-pseudo/recuperer-notification-par-pseudo.component';
+import { ListeEvenementPublicComponent } from './evenements/liste-evenement-public/liste-evenement-public.component';
+import { ListeEvenementPriveeComponent } from './evenements/liste-evenement-privee/liste-evenement-privee.component';
 
 const appRoutes: Routes = [
   {path: 'auth/inscription', component: InscriptionComponent},
@@ -29,7 +41,19 @@ const appRoutes: Routes = [
     HeaderComponent,
     EventListComponent,
     ListUsersComponent,
-    ListFriendsComponent
+    ListFriendsComponent,
+    CreateOpenDataEventComponent,
+    CreateEventPrivateComponent,
+    CreateSoireeComponent,
+    ListeParticipantSoireeComponent,
+    AjouterParticipantSoireeComponent,
+    SingleSoireeComponent,
+    AjouterEventPriveSoireeComponent,
+    AjouterEventOpendataSoireeComponent,
+    EnvoyerNotificationComponent,
+    RecupererNotificationParPseudoComponent,
+    ListeEvenementPublicComponent,
+    ListeEvenementPriveeComponent
   ],
   imports: [
     BrowserModule,
